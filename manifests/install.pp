f015a8f749a73e6fb20c41ab070798a605fe97dc
Kilian Engelhardt <kilian.engelhardt@godaddy.com>
2022-05-04 15:48:10 +0200

    add parameter to disable management of ferm package
    Enables using an installation method other than package or compiling from
    source. Before choosing either one was mandatory.

--- manifests/install.pp
+++ manifests/install.pp
@@ -7,39 +7,41 @@ class ferm::install {
   # this is a private class
   assert_private("You're not supposed to do that!")
 
-  case $ferm::install_method {
-    'package': {
-      package { 'ferm':
-        ensure => $ferm::package_ensure,
+  if $ferm::manage_package {
+    case $ferm::install_method {
+      'package': {
+        package { 'ferm':
+          ensure => $ferm::package_ensure,
+        }
       }
-    }
-    'vcsrepo': {
-      $_source_path = '/opt/ferm'
-      ensure_packages (['git', 'iptables', 'perl', 'make'], { ensure => present, before => Exec['make install'] })
+      'vcsrepo': {
+        $_source_path = '/opt/ferm'
+        ensure_packages (['git', 'iptables', 'perl', 'make'], { ensure => present, before => Exec['make install'] })
 
-      package { 'ferm':
-        ensure => absent,
+        package { 'ferm':
+          ensure => absent,
+        }
+        -> vcsrepo { $_source_path :
+          ensure   => present,
+          provider => git,
+          source   => $ferm::vcsrepo,
+          revision => $ferm::vcstag,
+        }
+        -> exec { 'make install':
+          cwd     => $_source_path,
+          path    => '/usr/sbin:/usr/bin:/sbin:/bin:/usr/bin/core_perl',
+          creates => '/usr/sbin/ferm',
+        }
+        -> file { '/etc/ferm':
+          ensure => directory,
+          owner  => 0,
+          group  => 0,
+          mode   => '0700',
+        }
       }
-      -> vcsrepo { $_source_path :
-        ensure   => present,
-        provider => git,
-        source   => $ferm::vcsrepo,
-        revision => $ferm::vcstag,
+      default: {
+        fail("unexpected install_method ${ferm::install_method}")
       }
-      -> exec { 'make install':
-        cwd     => $_source_path,
-        path    => '/usr/sbin:/usr/bin:/sbin:/bin:/usr/bin/core_perl',
-        creates => '/usr/sbin/ferm',
-      }
-      -> file { '/etc/ferm':
-        ensure => directory,
-        owner  => 0,
-        group  => 0,
-        mode   => '0700',
-      }
-    }
-    default: {
-      fail("unexpected install_method ${ferm::install_method}")
     }
   }
 }
