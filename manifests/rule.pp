903a469f693badacd2247b111b2cfcea3df41604
Kilian Engelhardt <kilian.engelhardt@godaddy.com>
2024-01-26 14:32:56 +0100

    align parameters in manifests/init.pp
    align parameters of defined type 'rule'
    
    align parameters in manifests/chain.pp
    
    update header for better readability

--- manifests/rule.pp
+++ manifests/rule.pp
@@ -1,7 +1,8 @@
 # @summary This defined resource manages a single rule in a specific chain
 #
 # @example Jump to the 'SSH' chain for all incoming SSH traffic (see chain.pp examples on how to create the chain)
-#   ferm::rule{'incoming-ssh':
+#
+#   ferm::rule{ 'incoming-ssh':
 #     chain  => 'INPUT',
 #     action => 'SSH',
 #     proto  => 'tcp',
@@ -9,7 +10,8 @@
 #   }
 #
 # @example Create a rule in the 'SSH' chain to allow connections from localhost
-#   ferm::rule{'allow-ssh-localhost':
+#
+#   ferm::rule{ 'allow-ssh-localhost':
 #     chain  => 'SSH',
 #     action => 'ACCEPT',
 #     proto  => 'tcp',
@@ -17,9 +19,9 @@
 #     saddr  => '127.0.0.1',
 #   }
 #
-#
 # @example Confuse people that do a traceroute/mtr/ping to your system
-#   ferm::rule{'drop-icmp-time-exceeded':
+#
+#   ferm::rule{ 'drop-icmp-time-exceeded':
 #     chain         => 'OUTPUT',
 #     action        => 'DROP',
 #     proto         => 'icmp',
@@ -27,45 +29,71 @@
 #   }
 #
 # @example allow multiple protocols
-#   ferm::rule{'allow_consul':
+#
+#   ferm::rule{ 'allow_consul':
 #     chain  => 'INPUT',
 #     action => 'ACCEPT',
 #     proto  => ['udp', 'tcp'],
 #     dport  => 8301,
 #   }
 #
-# @param chain Configure the chain where we want to add the rule
-# @param proto Which protocol do we want to match, typically UDP or TCP
-# @param comment A comment that will be added to the ferm config and to ip{,6}tables
+# @param chain
+#   Configure the chain where we want to add the rule
+#
+# @param proto
+#   Which protocol do we want to match, typically UDP or TCP
+#
+# @param comment
+#   A comment that will be added to the ferm config and to ip{,6}tables
+#
 # @param action Configure what we want to do with the packet (drop/accept/reject, can also be a target chain name). The parameter is mandatory.
 #   Allowed values: (RETURN|ACCEPT|DROP|REJECT|NOTRACK|LOG|MARK|DNAT|SNAT|MASQUERADE|REDIRECT|String[1])
-# @param dport The destination port, can be a single port number as integer or an Array of integers (which will then use the multiport matcher)
-# @param sport The source port, can be a single port number as integer or an Array of integers (which will then use the multiport matcher)
-# @param saddr The source address we want to match
-# @param daddr The destination address we want to match
-# @param proto_options Optional parameters that will be passed to the protocol (for example to match specific ICMP types)
-# @param interface an Optional interface where this rule should be applied
-# @param ensure Set the rule to present or absent
-# @param table Select the target table (filter/raw/mangle/nat)
+#
+# @param dport
+#   The destination port, can be a single port number as integer or an Array of integers (which will then use the multiport matcher)
+#
+# @param sport
+#   The source port, can be a single port number as integer or an Array of integers (which will then use the multiport matcher)
+#
+# @param saddr
+#   The source address we want to match
+#
+# @param daddr
+#   The destination address we want to match
+#
+# @param proto_options
+#   Optional parameters that will be passed to the protocol (for example to match specific ICMP types)
+#
+# @param interface
+#   an Optional interface where this rule should be applied
+#
+# @param ensure
+#   Set the rule to present or absent
+#
+# @param table
+#   Select the target table (filter/raw/mangle/nat)
 #   Default value: filter
 #   Allowed values: (filter|raw|mangle|nat) (see Ferm::Tables type)
-# @param negate Single keyword or array of keywords to negate
+#
+# @param negate
+#   Single keyword or array of keywords to negate
 #   Default value: undef
 #   Allowed values: (saddr|daddr|sport|dport) (see Ferm::Negation type)
+#
 define ferm::rule (
-  String[1] $chain,
-  Ferm::Protocols $proto,
-  Ferm::Actions $action,
-  String $comment = $name,
-  Optional[Ferm::Port] $dport = undef,
-  Optional[Ferm::Port] $sport = undef,
-  Optional[Variant[Array, String[1]]] $saddr = undef,
-  Optional[Variant[Array, String[1]]] $daddr = undef,
-  Optional[String[1]] $proto_options = undef,
-  Optional[String[1]] $interface = undef,
-  Enum['absent','present'] $ensure = 'present',
-  Ferm::Tables $table = 'filter',
-  Optional[Ferm::Negation] $negate = undef,
+  String[1]                           $chain,
+  Ferm::Protocols                     $proto,
+  Ferm::Actions                       $action,
+  String                              $comment       = $name,
+  Optional[Ferm::Port]                $dport         = undef,
+  Optional[Ferm::Port]                $sport         = undef,
+  Optional[Variant[Array, String[1]]] $saddr         = undef,
+  Optional[Variant[Array, String[1]]] $daddr         = undef,
+  Optional[String[1]]                 $proto_options = undef,
+  Optional[String[1]]                 $interface     = undef,
+  Enum['absent','present']            $ensure        = 'present',
+  Ferm::Tables                        $table         = 'filter',
+  Optional[Ferm::Negation]            $negate        = undef,
 ) {
   if $action in ['RETURN', 'ACCEPT', 'DROP', 'REJECT', 'NOTRACK', 'LOG', 'MARK', 'DNAT', 'SNAT', 'MASQUERADE', 'REDIRECT'] {
     $action_real = $action
