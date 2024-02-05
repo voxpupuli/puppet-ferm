13ef66843c9f65480e6ebb1459a292e0dddbcd1d
Kilian Engelhardt <kilian.engelhardt@godaddy.com>
2024-02-05 17:10:09 +0100

    add test for 'Ferm::Negation'

---
+++ spec/type_aliases/negation_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'Ferm::Negation' do
+  describe 'valid values' do
+    valid_values = %w[saddr daddr sport dport]
+
+    valid_values.each do |value|
+      describe value.inspect do
+        it { is_expected.to allow_value(value) }
+      end
+    end
+  end
+
+  describe 'invalid values' do
+    context 'with garbage inputs' do
+      [
+        'RETURN',
+        'REJECT',
+        'foobar',
+        :symbol,
+        nil,
+        '',
+        true,
+        false,
+        %w[meep meep],
+        65_538,
+        [95_000, 67_000],
+        {},
+        { 'foo' => 'bar' },
+      ].each do |value|
+        describe value.inspect do
+          it { is_expected.not_to allow_value(value) }
+        end
+      end
+    end
+  end
+end
